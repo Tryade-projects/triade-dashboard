a9f0a3c3048807bc7e0072e21aa33ef9eb4d14a9
DevWeb13 <giuliano.loic@sfr.fr>
2023-03-04 16:44:10 +0100

    feat: style sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Logo from '../../assets/logo.svg';
+import Home from '../../assets/home.svg';
+import Student from '../../assets/student.svg';
+
+import Finance from '../../assets/finance.svg';
+import Gear from '../../assets/gear.svg';
+import Chat from '../../assets/chat.svg';
+import PowerOff from '../../assets/power-off.svg';
 
 const Sidebar = () => {
-  const [activeLink, setActiveLink] = useState('');
-
-  const location = useLocation();
-
-  useEffect(() => {
-    setActiveLink(location.pathname);
-  }, [location]);
-
   return (
     <aside className='sidebar'>
       <div className='sidebarHeader'>
@@ -25,12 +24,24 @@ const Sidebar = () => {
         <NavLink
           to='/home'
           className='sidebarLink'>
+          <img
+            src={Home}
+            alt='accueil'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Accueil</span>
         </NavLink>
 
         <NavLink
           to='/employees'
           className='sidebarLink'>
+          <img
+            src={Student}
+            alt='employés'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Employés</span>
         </NavLink>
 
@@ -43,24 +54,48 @@ const Sidebar = () => {
         <NavLink
           to='/finance'
           className='sidebarLink'>
+          <img
+            src={Finance}
+            alt='finances'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Finances</span>
         </NavLink>
 
         <NavLink
           to='/improvements'
           className='sidebarLink'>
+          <img
+            src={Gear}
+            alt='améliorations'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Améliorations</span>
         </NavLink>
 
         <NavLink
           to='/activities'
           className='sidebarLink'>
+          <img
+            src={Chat}
+            alt='activités'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Activitées</span>
         </NavLink>
 
         <button
           type='button'
           className='sidebarLink'>
+          <img
+            src={PowerOff}
+            alt='déconnexion'
+            width={40}
+            height={40}
+          />
           <span className='titleLink'>Fermer</span>
         </button>
       </div>
