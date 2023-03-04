0a784551b884417ac21f672630cc50895b4692a6
DevWeb13 <giuliano.loic@sfr.fr>
2023-03-04 11:28:52 +0100

    feat: Add fonts Poppins + start sidebar style

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,47 +1,73 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import Logo from '../../assets/logo.svg';
 
 const Sidebar = () => {
+  const [activeLink, setActiveLink] = useState('');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location]);
+
   return (
     <aside className='sidebar'>
+      <div className='sidebarHeader'>
+        <img
+          src={Logo}
+          alt='Logo'
+        />
+        <p>Entreprise</p>
+      </div>
+
       <div className='sidebarMenu'>
-        <ul>
-          <li>
-            <NavLink
-              to='/home'
-              className='sidebarLink'>
-              <span>Home</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/employees'
-              className='sidebarLink'>
-              <span>Employees</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/finance'
-              className='sidebarLink'>
-              <span>Finance</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/improvements'
-              className='sidebarLink'>
-              <span>Improvements</span>
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/activities'
-              className='sidebarLink'>
-              <span>Activities</span>
-            </NavLink>
-          </li>
-        </ul>
+        <NavLink
+          to='/home'
+          className='sidebarLink'>
+          <span className='titleLink'>Accueil</span>
+        </NavLink>
+
+        <NavLink
+          to='/employees'
+          className='sidebarLink'>
+          <span className='titleLink'>Employés</span>
+        </NavLink>
+
+        <NavLink
+          to='/ranks'
+          className='sidebarLink'>
+          <span className='titleLink'>Grades</span>
+        </NavLink>
+
+        <NavLink
+          to='/finance'
+          className='sidebarLink'>
+          <span className='titleLink'>Finances</span>
+        </NavLink>
+
+        <NavLink
+          to='/improvements'
+          className='sidebarLink'>
+          <span className='titleLink'>Améliorations</span>
+        </NavLink>
+
+        <NavLink
+          to='/activities'
+          className='sidebarLink'>
+          <span className='titleLink'>Activitées</span>
+        </NavLink>
+
+        <button
+          type='button'
+          className='sidebarLink'>
+          <span className='titleLink'>Fermer</span>
+        </button>
+      </div>
+
+      <div className='sidebarFooter'>
+        <p>iManagement - Gestion d'entreprise</p>
+        <p>iFruit - Tous droits réservés</p>
       </div>
     </aside>
   );
---
+++ src/pages/Ranks/Ranks.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Ranks = () => {
+  return <div>Ranks</div>;
+};
+
+export default Ranks;
