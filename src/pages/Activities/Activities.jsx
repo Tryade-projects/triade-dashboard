a0d2b4f96a2022e92ab7ae35d9b335e44ff9fefd
DevWeb13 <giuliano.loic@sfr.fr>
2023-03-06 15:40:36 +0100

    feat: eslintrc prettierrc

--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import FormatIcon from '../../components/FormatIcon/FormatIcon';
-import StudentWhite from '../../assets/studentWhite.svg';
+import React from "react";
+import FormatIcon from "../../components/FormatIcon/FormatIcon";
+import StudentWhite from "../../assets/studentWhite.svg";
 
 const Activities = () => {
   return (
     <div>
-      <FormatIcon
-        background='#4D44B5'
-        image={StudentWhite}
-      />
+      <FormatIcon background="#4D44B5" image={StudentWhite} />
     </div>
   );
 };
