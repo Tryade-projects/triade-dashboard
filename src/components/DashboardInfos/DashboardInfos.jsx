48b5a1806d70aac46aa533ee709a6d7c2c576e36
Hababjiour <camstutzcore@gmail.com>
2023-03-07 14:21:35 +0100

    Créations du dashboard infos avec mise en forme. Changement de couleur du SVG via fill. Créations du fichier _mixin.scss et création de deux mixins pour les titre.

--- src/components/DashboardInfos/DashboardInfos.jsx
+++ src/components/DashboardInfos/DashboardInfos.jsx
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from 'react';
+import FormatIcon from "../../components/FormatIcon/FormatIcon";
+import Finance from "../../../src/assets/finance.svg";
 
 const DashboardInfos = () => {
   return (
-    <div>DashboardInfos</div>
+    <section className='dashboardInfos'>
+      
+      <div>
+        <div>
+          {/* insérer variable $colorPurple */}
+          <FormatIcon background="#4D44B5" image={Finance} />
+        </div>
+        <div>
+          <h3>Employés</h3>
+          <h2>49</h2>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {/* insérer variable $colorOrange */}
+          <FormatIcon background="#FB7D5B" image={Finance} />
+        </div>
+        <div>
+          <h3>Stock</h3>
+          <h2>754kg</h2>
+        </div>
+      </div>
+      
+      <div>
+        <div>
+          {/* insérer variable $colorYellow */}
+          <FormatIcon background="#FCC43E" image={Finance} />
+        </div>
+        <div>
+          <h3>Argent</h3>
+          <h2>320k</h2>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {/* insérer variable $colorText  */}
+          <FormatIcon background="#303972" image={Finance} />
+        </div>
+        <div>
+          <h3>Véhicules</h3>
+          <h2>40</h2>
+        </div>
+      </div>
+
+    </section>
+
+
   )
 }
 
