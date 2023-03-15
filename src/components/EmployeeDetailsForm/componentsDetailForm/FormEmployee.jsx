d4c3f7f173e798fdcdff24bc7962256c4fca88d0
Abel <josephine.abel@outlook.fr>
2023-03-15 23:38:16 +0100

    start css form

--- src/components/EmployeeDetailsForm/componentsDetailForm/FormEmployee.jsx
+++ src/components/EmployeeDetailsForm/componentsDetailForm/FormEmployee.jsx
@@ -5,33 +5,53 @@ const FormEmployee = () => {
   return (
     <form className={styles.form}>
       <div>
-        <label htmlFor="">Prenom</label>
-        <input type="text" />
+        <label className="semiBold" htmlFor="first-name">
+          Prenom *
+        </label>
+        <input id="first-name" type="text" placeholder="Samantha" />
       </div>
       <div>
-        <label htmlFor="">Nom</label>
-        <input type="text" />
+        <label className="semiBold" htmlFor="last-name">
+          Nom *
+        </label>
+        <input id="last-name" type="text" placeholder="William" />
       </div>
       <div>
-        <label htmlFor="">Date et Lieu de Naissance</label>
-        <input type="text" />
-        <input type="text" />
+        <label className="semiBold" htmlFor="date-place-born">
+          Date et Lieu de Naissance *
+        </label>
+        <div className={styles.wrapperDateBorn}>
+          <input
+            id="date-place-born"
+            type="text"
+            placeholder="24 Février 1997"
+          />
+          <input id="date-place-born" type="text" placeholder="Jakarta" />
+        </div>
       </div>
       <div>
-        <label htmlFor="">Adresse</label>
-        <input type="text" />
+        <label className="semiBold" htmlFor="address">
+          Adresse *
+        </label>
+        <input id="address" type="text" placeholder="856 Hilton Street" />
       </div>
       <div>
-        <label htmlFor="">Email</label>
-        <input type="text" />
+        <label className="semiBold" htmlFor="mail">
+          Email *
+        </label>
+        <input id="mail" type="text" placeholder="william@mail.com" />
       </div>
       <div>
-        <label htmlFor="">Phone</label>
-        <input type="number" />
+        <label className="semiBold" htmlFor="phone">
+          Phone *
+        </label>
+        <input id="phone" type="text" placeholder="555-1345" />
       </div>
       <div>
-        <label htmlFor="">Informations</label>
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <label className="semiBold" htmlFor="information">
+          Informations
+        </label>
+        <textarea name="" id="information" cols="30" rows="10"></textarea>
       </div>
     </form>
   );
