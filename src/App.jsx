d4434123e6c2c0a2a412e8102ae80872167dbb15
DevWeb13 <giuliano.loic@sfr.fr>
2023-04-04 10:58:33 +0200

    fix: (App.jsx): Delete useless props of <Ranks />

--- src/App.jsx
+++ src/App.jsx
@@ -50,8 +50,11 @@ function App() {
               path="/employees/*"
               element={<Employees employees={employees} />}
             />
-            <Route path="/ranks/*" element={<Ranks ranks={ranks} />} />
-            <Route path="/finance" element={<Finance employees={employees}/>} />
+            <Route path="/ranks/*" element={<Ranks />} />
+            <Route
+              path="/finance"
+              element={<Finance employees={employees} />}
+            />
             <Route path="/ranks/*" element={<Ranks />} />
             <Route path="/finance" element={<Finance />} />
             <Route path="/improvements" element={<Improvements />} />
