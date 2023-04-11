16248c7c4e7c2acad8e9e14184a3f853073ffc46
Hababjiour <camstutzcore@gmail.com>
2023-04-11 11:23:56 +0200

    Changement valeur en dur en valeur vh et vw sur app.scss, dashboardInfos

--- src/pages/Improvements/Improvements.jsx
+++ src/pages/Improvements/Improvements.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticleContainer from '../../components/ArticleContainer/ArticleContainer';
 import Header from '../../components/Header/Header';
+import ButtonsFilterWrapper from "../../components/ButtonsFilterWrapper/ButtonsFilterWrapper";
+import filteredData from "../../utils/filteredData";
+import { fetchData } from "../../utils/fetchData";
+
+
 
 const Improvements = () => {
 
-  return(
-  <div className='main mainImprovements'>
-    <Header title="Améliorations"/>
-    <ArticleContainer title="Améliorations" />
-  </div>
+  const improvementsLabels = [
+    "Stockage",
+    "Personnel",
+    "Garage"
+  ]
+
+  const [improvements, setImprovements] = useState([]);
+
+  const [category, setCategory] = useState("Stockage");
+
+  useEffect(() => {
+
+    fetchData("/fakeImprovements.json").then((data) => {
+      // @ts-ignore
+      setImprovements(filteredData(data, category, ["category"]));
+    });
+    // setCurrentPage(1);
+  }, [category]);
+
+
+  return (
+    <main className='main mainImprovements'>
+      <Header title="Améliorations" />
+      {/* <ArticleContainer title="Améliorations"  /> */}
+      {improvements.map((improvement) => (
+        <div>{improvement.category}</div>
+      ))}
+      <div className='contain'>
+        <ButtonsFilterWrapper labels={improvementsLabels} category={category} setCategory={setCategory} />
+      </div>
+    </main >
   )
 };
 
