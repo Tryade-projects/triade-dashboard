66a6f2cd59f70d50587fb669191a9d6982d0f75c
Hababjiour <camstutzcore@gmail.com>
2023-04-18 11:32:50 +0200

    Ajout de fonctions pour adapter les Level concernant les différentes catégories d'amélioration.

--- src/components/ButtonBuy/ButtonBuy.jsx
+++ src/components/ButtonBuy/ButtonBuy.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 /**
  * 
@@ -6,13 +6,34 @@ import React from "react";
  * @returns 
  */
 
-const ButtonBuy = ({text}) =>{
+// const [actualLvlImprovement, setActualLvlImprovement] = useState(0);
+const actualLvlImprovement = 1
 
-    return(
+const ButtonBuy = ({ level, actualLvlImprovement }) => {
+
+  function actualLvl() {
+    if (actualLvlImprovement + 1 === level) {
+      return (
         <div className="buttonBuy">
-            <p>{text}</p>
+          <p>Acheter</p>
         </div>
-    );
+      );
+    } else if (actualLvlImprovement >= level) {
+      return (
+        <div className="buttonBuy notSelectable">
+          <p>Possédé</p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="buttonBuy notSelectable">
+          <p>Niv. {level - 1} Requis</p>
+        </div>
+      );
+    }
+  }
+
+  return <div className="buttonBuy">{actualLvl()}</div>;
 };
 
 export default ButtonBuy;
--- src/pages/Improvements/Improvements.jsx
+++ src/pages/Improvements/Improvements.jsx
@@ -10,85 +10,109 @@ import PaginationEmployee from '../../components/PaginationEmployee/PaginationEm
 import star from "../../assets/star.svg"
 import capacity from "../../assets/capacity.svg"
 import arrow from "../../assets/arrow.svg"
+import fakeDataCompany from "../../../fakeCompanyData.json";
 
 const Improvements = () => {
 
+  // State that holds the improvements data and the selected category for filtering
   const [improvements, setImprovements] = useState([]);
   const [category, setCategory] = useState("Stockage");
+
+  // Number of improvements displayed per page
   const infoPerPage = 3;
 
+  // Calculate the total number of pages based on the number of improvements and the infoPerPage value
   const numberPages = Math.ceil(improvements.length / infoPerPage);
 
-
-
-
-  //Utilisation du hook de pagination
+  // Hook for pagination functionality
   const { currentPage, setCurrentPage, nextPage, previousPage, paginate } =
     usePagination(numberPages, 1);
 
+  // Create an array of labels for filtering
   const improvementsLabels = [
     "Stockage",
     "Personnel",
     "Garage"
-  ]
+  ];
 
-
+  // Set the current page to the first page whenever the category changes
   useEffect(() => {
     setCurrentPage(1);
   }, [category]);
 
+  // Fetch the improvements data and apply the selected filter
   useEffect(() => {
-
     fetchData("/fakeImprovements.json").then((data) => {
-      // @ts-ignore
       setImprovements(filteredData(data, category, ["category"]));
     });
-    // setCurrentPage(1);
   }, [category]);
 
+  // Calculate the first and last indices of the current page of improvements
   const { firstIndex, lastIndex } = useIndexRange(currentPage, infoPerPage);
+
+  // Slice the improvements array to get the current page's improvements
   const currentImprovements = improvements.slice(firstIndex, lastIndex);
+
+  /////////
+  const [companyData, setCompanyData] = useState({});
+
+  useEffect(() => {
+    setCompanyData(fakeDataCompany); // Charger les données JSON du fichier
+  }, []);
+
+  function getActualLevel(category) {
+    return companyData.improvement[0][`actualLvlImprovement${category}`];
+  }
+  
+
 
 
   return (
     <main className='main mainImprovements'>
       <Header title="Améliorations" />
+
+      {/* Render the ArticleContainer component */}
       <ArticleContainer title="Améliorations"
         titleContain={<ButtonsFilterWrapper labels={improvementsLabels} category={category} setCategory={setCategory} />}
         contain={
           <>
             <div className='mainContain'>
-              {
-                currentImprovements.map((improvement, id) => (
-                  <div key={id} className='improvementContainMap'>
-                    <div className='improvementImage'>{improvement.image}</div>
-                    <div className='flexColumn'>
-                      <h2>{improvement.title}</h2>
-                      <h4>{category}</h4>
-                    </div>
-                    <div className='flexGrow2'>
-                      <div className="improvement"><img src={star} alt="" /></div>
-                      <h2>Niv.{improvement.level}</h2>
-                    </div>
-                    <div className='flexGrow2'>
-                      <div><img src={capacity} alt="" /></div>
-                      <div className='flexColumn'>
-                        <h2>{improvement.capacity}</h2>
-                        <h4>Capacité</h4>
-                      </div>
-                    </div>
-                    <div className='flexGrow2'>
-                      <div><img src={arrow} alt="" /></div>
-                      <div className='flexColumn'>
-                        <h2>{improvement.capacity}</h2>
-                        <h4>Capacité</h4>
-                      </div>
-                    </div>
-                    <ButtonBuy text="Acheter" />
+
+              {/* Map through the current improvements and render each one in a div */}
+              {currentImprovements.map((improvement, id) => (
+                <div key={id} className='improvementContainMap'>
+                  <div className='improvementImage'>{improvement.image}</div>
+                  <div className='flexColumn'>
+                    <h2>{improvement.title}</h2>
+                    <h4>{category}</h4>
                   </div>
-                ))}
+                  <div className='flexGrow2'>
+                    <div className="improvement"><img src={star} alt="" /></div>
+                    <h2>Niv.{improvement.level}</h2>
+                  </div>
+                  <div className='flexGrow2'>
+                    <div><img src={capacity} alt="" /></div>
+                    <div className='flexColumn'>
+                      <h2>{improvement.capacity}</h2>
+                      <h4>Capacité</h4>
+                    </div>
+                  </div>
+                  <div className='flexGrow2'>
+                    <div><img src={arrow} alt="" /></div>
+                    <div className='flexColumn'>
+                      <h2>{improvement.capacity}</h2>
+                      <h4>Capacité</h4>
+                    </div>
+                  </div>
+                  {/* Render the ButtonBuy component and pass in relevant props */}
+                  <ButtonBuy level={improvement.level} actualLvlImprovement={getActualLevel(category)} />
+
+                </div>
+              ))}
+
             </div>
 
+            {/* Render the PaginationEmployee component */}
             <div className="footer-dashboard-employee">
               <PaginationEmployee
                 infoPerPage={infoPerPage}
@@ -101,6 +125,7 @@ const Improvements = () => {
                 itemName="amélioration"
               />
             </div>
+
           </>
         }
       />
