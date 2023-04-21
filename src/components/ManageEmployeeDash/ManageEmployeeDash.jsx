2deba06d523ee9c4e2ca31eebb97b6c3022c5a5d
Abel <josephine.abel@outlook.fr>
2023-04-21 18:52:43 +0200

    reduce width pagination

--- src/components/ManageEmployeeDash/ManageEmployeeDash.jsx
+++ src/components/ManageEmployeeDash/ManageEmployeeDash.jsx
@@ -6,7 +6,7 @@ import filteredData from "../../utils/filteredData";
 import { useStickyState } from "../../utils/useStickyState";
 import PaginationEmployee from "../PaginationEmployee/PaginationEmployee";
 
-const INFO_PER_PAGE = 5;
+const INFO_PER_PAGE = 1;
 
 const ManageEmployeeDash = ({ search }) => {
   const [employees, setEmployees] = useStickyState("employees", []);
