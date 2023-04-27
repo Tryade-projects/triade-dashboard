014818d9822200f6f10590ecdac0c6cffc194b60
Hababjiour <camstutzcore@gmail.com>
2023-04-27 15:14:51 +0200

    Add titles on the details employee page and height modification of title CSS

--- src/components/EmployeeDetails/componentsEmployeeDetails/WrapperAvatarActions.jsx
+++ src/components/EmployeeDetails/componentsEmployeeDetails/WrapperAvatarActions.jsx
@@ -19,17 +19,17 @@ const WrapperAvatarActions = ({ profil }) => {
       </div>
       <div className={styles.actions}>
         <div>
-          <ButtonTransaction icon={trending} alt={"image pour augmenter"} />
+          <ButtonTransaction icon={trending} alt={"Bouton pour promouvoir l'employé"} title={"Promouvoir"} />
         </div>
         <div>
-          <ButtonTransaction icon={decrease} alt={"image pour diminuer"} />
+          <ButtonTransaction icon={decrease} alt={"Bouton pour rétrograder l'employé"} title={"Rétrograder"} />
         </div>
         <div>
-          <ButtonTransaction icon={fired} alt={"image pour virer"} />
+          <ButtonTransaction icon={fired} alt={"Bouton pour licensier l'employé"} title={"Licencier"} />
         </div>
         <div>
           <Link to={`/employees/employee/add/${profil.id}`} state={profil}>
-            <ButtonTransaction icon={gear} alt={"image pour paramétrer"} />
+            <ButtonTransaction icon={gear} alt={"Bouton pour voir les détails de l'employé"} title={"Modifier"}  />
           </Link>
         </div>
       </div>
