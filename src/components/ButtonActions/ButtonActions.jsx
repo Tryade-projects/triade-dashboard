3fa236427cc2e7558071eb1db93138902a47280e
Hababjiour <camstutzcore@gmail.com>
2023-04-27 13:36:06 +0200

    Add title on ButtonActions componenent, add CSS, homogenization of gear Icon on the boards

--- src/components/ButtonActions/ButtonActions.jsx
+++ src/components/ButtonActions/ButtonActions.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ButtonTransaction = ({ icon, alt, onClick }) => {
+const ButtonTransaction = ({ icon, alt, onClick, title }) => {
   return (
-    <button type="button" className="buttonActions" onClick={onClick}>
+    <button type="button" className="buttonActions" onClick={onClick} data-content={title}>
       <img src={icon} alt={alt} />
     </button>
   );
